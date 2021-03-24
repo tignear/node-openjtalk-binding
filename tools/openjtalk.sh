643263682a4bbef47cc0669185dd6f69582b52d0
tignear <tignear+m@gmail.com>
2021-03-24 23:26:11 +0900

    fix?

--- tools/openjtalk.sh
+++ tools/openjtalk.sh
@@ -1,5 +1,5 @@
 set -eu
 hts_engine_dir=`pwd`/htsengine
 cd openjtalk
-./configure --with-hts-engine-header-path=${hts_engine_dir}/include --with-hts-engine-library-path=${hts_engine_dir}/lib --with-charset=UTF-8 
+./configure --with-hts-engine-header-path=${hts_engine_dir}/include --with-hts-engine-library-path=${hts_engine_dir}/lib --with-charset=UTF-8 --disable-dependency-tracking
 make
