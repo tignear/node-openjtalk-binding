841a8442ab2e93449c7f38aff3e77d0af2d5b8c0
tignear <tignear+m@gmail.com>
2021-03-26 21:08:47 +0900

    fix nullablility in d.ts

--- addon.d.ts
+++ addon.d.ts
@@ -12,47 +12,47 @@ export type OpenJTalkOptions = {
     /**
      * Must be int. 1<=sampling_frequency.
      */
-    sampling_frequency: number | null;
+    sampling_frequency?: number | null;
     /**
      * Must be int. 1<=frame_period.
      */
-    frame_period: number | null;
+    frame_period?: number | null;
     /**
      * 0.0<=all_pass_constant<=1.0.
      */
-    all_pass_constant: number | null;
+    all_pass_constant?: number | null;
     /**
      * Default is 0.0. 0.0<=postfiltering_coefficient<=1.0.
      */
-    postfiltering_coefficient: number | null;
+    postfiltering_coefficient?: number | null;
     /**
      * Default is 1.0. 0<=speech_speed_rate. Warning: Do not set a very small value as it consumes CPU time.
      */
-    speech_speed_rate: number | null;
+    speech_speed_rate?: number | null;
     /**
      * Default is 0.0.
      */
-    additional_half_tone: number | null;
+    additional_half_tone?: number | null;
     /**
      * Default is 0.5. 0.0<=voiced_unvoiced_threshold<=1.0.
      */
-    voiced_unvoiced_threshold: number | null;
+    voiced_unvoiced_threshold?: number | null;
     /**
      * Default is 1.0. 0.0<=weight_of_GV_for_spectrum.
      */
-    weight_of_GV_for_spectrum: number | null;
+    weight_of_GV_for_spectrum?: number | null;
     /**
      * Default is 1.0. 0.0<=weight_of_GV_for_log_F0.
      */
-    weight_of_GV_for_log_F0: number | null;
+    weight_of_GV_for_log_F0?: number | null;
     /**
      * Default is 0.0.
      */
-    volume_in_dB: number | null;
+    volume_in_dB?: number | null;
     /**
      * Disabled as default. 0 regarded disabled. 0<=audio_buffer_size
      */
-    audio_buffer_size: number | null;
+    audio_buffer_size?: number | null;
 };
 export type WaveObject = {
     /**
