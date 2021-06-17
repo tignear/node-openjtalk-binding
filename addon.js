2930397e9360dced501792543e31ee8edf261bb2
Sueqk <68449029+Sueqkjs@users.noreply.github.com>
2021-06-18 08:22:55 +0900

    これはどう
    falsyな値を全て弾いて見た

--- addon.js
+++ addon.js
@@ -95,6 +95,7 @@ exports.synthesis = async function synthesis(text, options) {
   } else {
     dictionary = Object.fromEntries(Object.entries(dictionary).map(([k, v]) => [k, v instanceof Uint8Array ? v.buffer : v]));
   }
+  if (!text) throw new TypeError("The first argument must be a non-empty string");
   return new Promise((resolve, reject) => {
     function cb(err, /** @type {Buffer} */ buffer, /** @type {number} */ sampleRate) {
       if (err) {
